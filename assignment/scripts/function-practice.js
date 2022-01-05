9b23a2e0aa9d72c8fe21a2e16a604295ac93efe2
Tony LaForgia <alforgia428@gmail.com>
2022-01-05 03:42:09 -0600

    Completed Question 5

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -37,10 +37,11 @@ console.log(multiplyThree());
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
   }
-    return;
+    return false;
 }
+console.log(isPositive());
 // Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
